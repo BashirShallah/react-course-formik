432e54fe02402e30698dcb8b11f9e6f2f1c7a4ca
Bashir Shallah <bashir.shallah@gmail.com>
2018-12-15 12:22:05 +0300

    Field component type.

--- src/App.js
+++ src/App.js
@@ -12,7 +12,20 @@ class App extends Component {
       <label>Name</label><br />
       <Field name="name" /><br />
       <label>Email</label><br />
-      <Field name="email" /><br />
+      <Field name="email" type="email" /><br />
+      <label>Type</label><br />
+      <Field name="type" component="select">
+        <option value="1">One</option>
+        <option value="2">Two</option>
+      </Field>
+      <br />
+      <label>Active</label><br />
+      <Field name="active" type="checkbox" /><br />
+      <br />
+      <label>Category</label><br />
+      <Field name="category" type="radio" value="1" /> 1<br />
+      <Field name="category" type="radio" value="2" /> 2<br />
+
       <button type="submit">Send</button>
     </form>
   }
@@ -21,7 +34,7 @@ class App extends Component {
     return (
       <div className="App">
         <Formik 
-          initialValues={{name: "bashir", email: ""}}
+          initialValues={{name: "bashir", email: "", type: "",active: false,category: ""}}
           onSubmit={this.onSubmit}
           render={this.form}
           />
