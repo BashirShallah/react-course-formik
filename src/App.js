7f985f994401d75faf0b5cdcbf620544159f788b
Bashir Shallah <bashir.shallah@gmail.com>
2018-12-17 21:42:42 +0300

    validate Formik forms with Yup.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import {Formik , Field} from "formik";
+import {Formik , Field, ErrorMessage} from "formik";
+import * as Yup from "yup";
 
 class App extends Component {
   
@@ -11,23 +12,42 @@ class App extends Component {
     return <form onSubmit={props.handleSubmit}>
       <label>Name</label><br />
       <Field name="name" /><br />
+      <ErrorMessage name="name" /><br />
+
       <label>Email</label><br />
       <Field name="email" type="email" /><br />
+      <ErrorMessage name="email" /><br />
+
       <label>Type</label><br />
       <Field name="type" component="select">
         <option value="1">One</option>
         <option value="2">Two</option>
       </Field>
       <br />
+      <ErrorMessage name="type" /><br />
+
       <label>Active</label><br />
       <Field name="active" type="checkbox" /><br />
       <br />
+
       <label>Category</label><br />
       <Field name="category" type="radio" value="1" /> 1<br />
       <Field name="category" type="radio" value="2" /> 2<br />
+      <ErrorMessage name="category" /><br />
 
       <button type="submit">Send</button>
     </form>
+  }
+
+  schema = ()=>{
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      type: Yup.string().required(),
+      email: Yup.string().required(),
+      category: Yup.string().required(),
+    });
+
+    return schema;
   }
 
   render() {
@@ -37,6 +57,7 @@ class App extends Component {
           initialValues={{name: "bashir", email: "", type: "",active: false,category: ""}}
           onSubmit={this.onSubmit}
           render={this.form}
+          validationSchema={this.schema()}
           />
       </div>
     );
